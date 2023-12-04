8a68bf0cac5c12a81e02e8dec77a98b10b69abed
justyn temme <justyntemme@gmail.com>
2023-12-04 13:21:06 -0500

    sleep while we delete node groups

--- deleteStaleResources/main.py
+++ deleteStaleResources/main.py
@@ -1,4 +1,5 @@
 import boto3
+import time
 from datetime import datetime, timedelta, timezone
 import logging
 
@@ -12,33 +13,30 @@ def calculate_age_in_hours(creation_time_utc, current_time_utc):
 
 
 def get_eks_cluster_tags(eks_client, cluster_name):
-    # Retrieve EKS cluster tags
     account_id = boto3.client("sts").get_caller_identity().get("Account")
     region = boto3.session.Session().region_name
 
     resource_arn = f"arn:aws:eks:{region}:{account_id}:cluster/{cluster_name}"
     response = eks_client.list_tags_for_resource(resourceArn=resource_arn)
+
+    print("Response from list_tags_for_resource:")
+    print(response)
+
     return response.get("tags", {})
 
 
 def lambda_handler(event, context):
     try:
         # Set the region to your AWS region
-        region = context.invoked_function_arn.split(":")[
-            3
-        ]  # Extract region from Lambda ARN
+        region = context.invoked_function_arn.split(":")[3]
 
-        # Initialize AWS clients for EC2 and EKS
         ec2_client = boto3.client("ec2", region_name=region)
         eks_client = boto3.client("eks", region_name=region)
 
-        # Get current time in UTC
         current_time_utc = datetime.now(timezone.utc)
 
-        # Calculate the threshold time (24 hours ago)
         threshold_time_utc = current_time_utc - timedelta(hours=24)
 
-        # Check EC2 instances
         ec2_instances = ec2_client.describe_instances()
         for reservation in ec2_instances["Reservations"]:
             for instance in reservation["Instances"]:
@@ -60,7 +58,7 @@ def lambda_handler(event, context):
                 else:
                     if launch_time_utc < threshold_time_utc:
                         print(f"Deleting EC2 instance {instance_id}")
-                        # ec2_client.terminate_instances(InstanceIds=[instance_id])
+                        ec2_client.terminate_instances(InstanceIds=[instance_id])
                     else:
                         print(
                             f"EC2 instance {instance_id} is not older than 24 hours but can be deleted"
@@ -69,12 +67,10 @@ def lambda_handler(event, context):
                             f"Instance ID: {instance_id}, Tags: {tags}, Age: {age_in_hours:.2f} hours"
                         )
 
-        # Check EKS clusters
         eks_clusters = eks_client.list_clusters()
         for cluster_name in eks_clusters["clusters"]:
             cluster_tags = get_eks_cluster_tags(eks_client, cluster_name)
 
-            # Extract the creation time directly as a datetime object
             cluster_details = eks_client.describe_cluster(name=cluster_name)
             created_at_utc = cluster_details["cluster"]["createdAt"].replace(
                 tzinfo=timezone.utc
@@ -82,12 +78,8 @@ def lambda_handler(event, context):
 
             age_in_hours = calculate_age_in_hours(created_at_utc, current_time_utc)
 
-            # Extract tags from the tags list, if available
-            tags = (
-                cluster_tags  # Adjust this line based on the structure of your response
-            )
+            tags = cluster_tags
 
-            # Check for "do-not-delete" tag
             do_not_delete = tags.get("do-not-delete", "").lower()
             if do_not_delete == "true":
                 print(
@@ -98,9 +90,22 @@ def lambda_handler(event, context):
                 )
             else:
                 if created_at_utc < threshold_time_utc:
+                    # Check for attached node groups
+                    nodegroups = eks_client.list_nodegroups(clusterName=cluster_name)
+                    if nodegroups["nodegroups"]:
+                        # Delete attached node groups first
+                        for nodegroup_name in nodegroups["nodegroups"]:
+                            print(
+                                f"Deleting node group {nodegroup_name} for cluster {cluster_name}"
+                            )
+                            eks_client.delete_nodegroup(
+                                clusterName=cluster_name, nodegroupName=nodegroup_name
+                            )
+                            print("Waiting for node group deletion to complete...")
+                            time.sleep(60)
                     print(f"Deleting EKS cluster {cluster_name}")
                     # Delete EKS cluster - Note: This will also delete associated resources
-                    # eks_client.delete_cluster(name=cluster_name)
+                    eks_client.delete_cluster(name=cluster_name)
                 else:
                     print(
                         f"EKS cluster {cluster_name} is not older than 24 hours but can be deleted"
